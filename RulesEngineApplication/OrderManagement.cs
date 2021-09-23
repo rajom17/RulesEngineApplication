224b477b4cfcae6e7a4439032cbc4d1686e85730
raj shivraj <rajshivraj1717@gmail.com>
2021-09-23 11:42:01 +0530

    Add project files.

---
+++ RulesEngineApplication/OrderManagement.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RulesEngineApplication
+{
+    public class OrderManagement
+    {
+        public OrderManagement()
+        {
+
+        }
+    }
+}
---
+++ RulesEngineApplication/RuleEngine.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace RulesEngineApplication
+{
+    public class RuleEngine: IRuleEngine
+    {
+
+       
+    }
+}
