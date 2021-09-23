da2d029679237697922327af34336a6899fd4794
raj shivraj <rajshivraj1717@gmail.com>
2021-09-23 21:37:25 +0530

    main file try points

--- RulesEngineApplication/OrderManagement.cs
+++ RulesEngineApplication/OrderManagement.cs
@@ -6,10 +6,21 @@ namespace RulesEngineApplication
 {
     public class OrderManagement
     {
-        public OrderManagement()
+        private readonly IRuleEngine _ruleEngine;
+
+        public OrderManagement(IRuleEngine ruleEngine)
         {
+            _ruleEngine = ruleEngine;
+        }
 
-
+        public bool callRulesEngineBasedOnPaymetType(string paymentType)
+        {
+            var Result = false;
+            if(paymentType== "Book")
+            {
+                Result= _ruleEngine.paymentForBook();
+            }
+            return Result;
         }
     }
 }
---
+++ RulesEngineApplication/Program.cs
@@ -0,0 +1,13 @@
+﻿using RulesEngineApplication;
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Order Management Rule Engine");
+        }
+    }
+}
